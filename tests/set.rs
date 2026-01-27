a86bcd3ee15578aafce3614028b1fb245e152b87
Sup#2.0 <sup2point0@gmail.com>
2026-01-27 21:36:32 +0000

    dev - add tests

--- tests/set.rs
+++ tests/set.rs
@@ -34,3 +34,38 @@ use natbitset::*;
     assert!( !bitset.insert(1) );
     assert_eq!( bitset, byteset![1] );
 }
+
+#[test] fn is_subset()
+{
+    assert!( byteset![].is_subset(&byteset![]) );
+    assert!( byteset![].is_subset(&byteset![1]) );
+    assert!( byteset![].is_subset(&byteset![1;8]) );
+
+    assert!( byteset![1].is_subset(&byteset![1]) );
+    assert!( byteset![1].is_subset(&byteset![1;8]) );
+
+    assert!( byteset![1;7].is_subset(&byteset![1;8]) );
+    assert!( byteset![1;8].is_subset(&byteset![1;8]) );
+}
+
+#[test] fn is_superset()
+{
+    assert!( byteset![].is_superset(&byteset![]) );
+    assert!( byteset![1].is_superset(&byteset![]) );
+    assert!( byteset![1;8].is_superset(&byteset![]) );
+    assert!( byteset![1].is_superset(&byteset![1]) );
+    assert!( byteset![1;8].is_superset(&byteset![1]) );
+    assert!( byteset![1;8].is_superset(&byteset![1;7]) );
+    assert!( byteset![1;8].is_superset(&byteset![1;8]) );
+}
+
+#[test] fn is_disjoint()
+{
+    assert!( byteset![1].is_disjoint(&byteset![2]) );
+    assert!( byteset![2].is_disjoint(&byteset![1]) );
+
+    assert!( byteset![1,2].is_disjoint(&byteset![3,4]) );
+    assert!( byteset![3,4].is_disjoint(&byteset![1,2]) );
+
+    assert!( !byteset![1,2].is_disjoint(&byteset![2,3]) );
+}
